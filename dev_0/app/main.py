eb7f4327ad3b23cc55a349f605e38bf2985b5611
m-weinzettl <054057@edu.szf.at>
2026-01-29 11:19:37 +0100

    test angelegt

---
+++ dev_0/app/main.py
@@ -0,0 +1,13 @@
+import test
+import markus
+
+print("tst")
+
+liste = [1, 2, 3, 4, 5]
+
+liste = (x * 2 for x in liste)
+
+dictionary = {x: x * 2 for x in range(5)}
+
+print(test.show_test())
+
---
+++ dev_0/app/test.py
@@ -0,0 +1,2 @@
+def show_test():
+    print("Hello, World!")
